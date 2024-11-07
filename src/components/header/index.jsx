fbcb1ef083a0a5b20a3bac71e762d8e9601edab0
Hrach92 <hrach.gh.92@mail.ru>
2024-11-07 14:38:21 +0400

    deleted unused components

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -5,7 +5,6 @@ import Dropdown from "../_shared/dropdown";
 const Header = () => {
   return (
     <div className={styles.container}>
-      <button>HOme</button>
       <Dropdown />
     </div>
   );
